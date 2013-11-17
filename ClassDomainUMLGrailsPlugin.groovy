72e9b81cf53f1fd5da83f9a9a75de818fde5cc03
Ignacio Ocampo <ignacio.ocampo@desarrollointeractivo.com>
2013-11-17 02:32:44 -0600

    Plugin configuration, licence, source code and issue page

--- ClassDomainUMLGrailsPlugin.groovy
+++ ClassDomainUMLGrailsPlugin.groovy
@@ -22,7 +22,7 @@ Automagically create a UML diagram from all your domain classes of your Grails a
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
     def organization = [ name: "Nafiux", url: "http://www.nafiux.com/" ]
@@ -31,10 +31,10 @@ Automagically create a UML diagram from all your domain classes of your Grails a
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "JIRA", url: "https://github.com/nafiux/grais-plugin-class-domain-uml/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/nafiux/grais-plugin-class-domain-uml" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
